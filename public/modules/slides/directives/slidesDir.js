f2d739afe589113a571cd6f52d55af93be910ff7
Jamie Holliday <jamie.holliday@vaimo.com>
2014-12-08 13:33:27 +0000

    Update presentation

--- public/modules/slides/directives/slidesDir.js
+++ public/modules/slides/directives/slidesDir.js
@@ -15,14 +15,17 @@ presentationApp.directive('jhSlidedeck', function($state, $window, $document) {
 			scope.next = function() {
 				if(scope.currentIndex < scope.slides.length) {
 					scope.currentIndex++;
-					$window.location.href = '#/slides/'+ scope.currentIndex;
+					//$window.location.href = '#/slides/'+ scope.currentIndex;
+					//history.pushState(null, null, '#/slides/'+ scope.currentIndex);
 				}
 			};
 
 			scope.prev = function() {
 				if(scope.currentIndex > 1) {
 					scope.currentIndex--;
-					$window.location.href = '#/slides/'+ scope.currentIndex;
+					//$window.location.href = '#/slides/'+ scope.currentIndex;
+					//history.pushState(null, null, '#/slides/'+ scope.currentIndex);
+
 				}
 			};
 
@@ -33,13 +36,13 @@ presentationApp.directive('jhSlidedeck', function($state, $window, $document) {
 				scope.slides[scope.currentIndex - 1].visible = true;
 			});
 
-			$document.bind('keyup', function(evt) {
-				if(evt.keyCode === 39) {
-					scope.next();
-				} else if(evt.keyCode === 37) {
-					scope.prev();
-				}
-			});
+			// $document.bind('keyup', function(evt) {
+			// 	if(evt.keyCode === 39) {
+			// 		scope.next();
+			// 	} else if(evt.keyCode === 37) {
+			// 		scope.prev();
+			// 	}
+			// });
 		},
 		templateUrl: 'partials/slideContainer.html'
 	};
